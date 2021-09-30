64871dfd6a07e6e5759fa70006e460af3f00167e
MrigElcoder <88018113+MrigElcoder@users.noreply.github.com>
2021-09-30 20:57:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 var http = require("http");
 
 const httpServer = http.createServer(handleServer);
-
+const host = 'localhost';
+const port = 8000;
 
 function handleServer(req, res) {
   res.setHeader("Content-Type","text/html");
   res.writeHead(200);
   res.end(" Welcome to Dominos!");
 }
-server.listen(8081);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
 module.exports = httpServer;
