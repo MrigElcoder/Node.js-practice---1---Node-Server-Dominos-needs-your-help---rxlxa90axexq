7c8ab29f59ef6c80e23131e03870c0455ae066c5
MrigElcoder <88018113+MrigElcoder@users.noreply.github.com>
2021-09-30 20:53:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  res.setHeader("Content-Type","text/html");
+  res.writeHead(200);
+  res.end(" Welcome to Dominos!");
 }
 
 module.exports = httpServer;
